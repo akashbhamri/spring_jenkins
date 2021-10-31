be8ce394031f8a7bc02570eb7c359eec76e2faaa
akashbhamri <akashbhamri741@gmail.com>
2021-10-31 12:50:19 +0530

    add

--- src/test/java/com/jenkins/api/SpringJenkinsApplicationTests.java
+++ src/test/java/com/jenkins/api/SpringJenkinsApplicationTests.java
@@ -1,13 +1,26 @@
 package com.jenkins.api;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringJenkinsApplicationTests {
 
+	
+	 public static org.slf4j.Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		logger.info("Test case executing");
+		logger.info("Test case executing second log ");
+		Assertions.assertEquals(true, true);
+	
+
 	}
+
+	
 
 }
